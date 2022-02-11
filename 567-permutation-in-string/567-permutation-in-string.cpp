fa43f727f849a0dbd126dcaa6b22971154583574
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2022-02-11 23:36:58 +0530

    Time: 17 ms (35.50%), Space: 7.4 MB (25.75%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map< char, int >mp;    //Use hashmap to store the frequencies of all the characters present in string s1.
+        unordered_map< char, int >mp;   
         for(auto it : s1){
             mp[it]++;
         }
-        int count = mp.size();         //Use the count variable to see if all of the characters in the map have the same frequency, indicating that an anagram has been found.
+        int count = mp.size();     
         int i = 0, j = 0;
-        int k = s1.size();             //Window Size
+        int k = s1.size();             
         while(j < s2.size()){
-            if(mp.find(s2[j]) != mp.end()){      //If a character is found that already exists in the map, reduce its frequency by one.
+            if(mp.find(s2[j]) != mp.end()){   
                 mp[s2[j]]--;
-                if(mp[s2[j]] == 0){     //If the frequency of a specific character on the map is 0, it means that all occurrences of that character is found inside the current window size.
+                if(mp[s2[j]] == 0){    
                     count--;
                 }
             }
@@ -19,10 +19,10 @@ public:
                 j++;
             }
             else if(j-i+1 == k){
-				if(count == 0){    //Anagram found 
+				if(count == 0){     
 					return true;  
 				}
-                if(mp.find(s2[i]) != mp.end()){  //Check if that character is present in the map while sliding the window, then increase its frequency by one, as we decreased its frequency when we first met it while crossing the window.
+                if(mp.find(s2[i]) != mp.end()){  
                     mp[s2[i]]++;
                     if(mp[s2[i]] == 1){
                         count++;
