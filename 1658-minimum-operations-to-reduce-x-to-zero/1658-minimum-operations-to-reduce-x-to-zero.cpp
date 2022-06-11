a7b735017302ea5225fd9465b91d2f6944c5adbe
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2022-06-11 16:34:45 +0530

    Time: 269 ms (60.97%), Space: 98.6 MB (71.00%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++) {
+            sum+=nums[i];
+        }
+        int t_sum = sum-x;
+        if(t_sum==0) {
+            return nums.size();
+        } else if (t_sum<0) {
+            return -1;
+        }
+        int ans = 0;
+        int s=0, e=0, c_sum=nums[0];
+        while(e<nums.size()) {
+            if(c_sum<t_sum) {
+                e++;
+                if(e==nums.size()) break;
+                c_sum+=nums[e];
+            } else if(c_sum>t_sum) {
+                c_sum-= nums[s];
+                s++;
+            } else if(c_sum==t_sum) {
+                ans = max(ans, e-s+1);
+                c_sum-=nums[s];
+                s++;
+            }
+        }
+        if(ans==0) return -1;
+        return nums.size()-ans;
+    }
+};
+
+// 11 , x=5
+// ans = 2
+// min of nums elements to be picked to get sum 5
+// max of nums elements to be picked to get sum 11-5
