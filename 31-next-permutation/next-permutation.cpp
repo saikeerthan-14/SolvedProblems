0a89bd176a89d82f4d570fe888395a5b4b3cb59f
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2024-03-31 16:02:33 -0400

    Time: 4 ms (58.31%) | Memory: 14.5 MB (57.54%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-2;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;
+        cout<<i<<endl;
+        if(i<0) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        else {
+            cout<<"enter"<<endl;
+            for(int j=n-1;j>i;j--) {
+                if(nums[j]>nums[i]) {
+                    swap(nums[j], nums[i]);
+                    break;
+                }
+            }
+            reverse(nums.begin()+i+1, nums.end());
+        }
+        // int idx = -1;
+        // for(int i = n-2;i>=0;i--) {
+        //     if(nums[i]<nums[i+1]) {
+        //         idx = i;
+        //         break;
+        //     }
+        // }
+        // cout<<idx<<endl;
+        // if(idx==-1) {
+        //     reverse(nums.begin(), nums.end());
+        //     return;
+        // }
+        // for(int i = n-1;i>idx;i--) {
+        //     if(nums[i]>nums[idx])  {
+        //         swap(nums[i], nums[idx]);
+        //         break;
+        //     }
+        // }
+        // reverse(nums.begin()+idx+1, nums.end());
+        // return nums;
+    }
+};
