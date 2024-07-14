715a5ef913f8b3f623a181013f3596749492f1be
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2024-07-13 18:58:42 -0700

    Time: 23 ms (76.27%) | Memory: 24.9 MB (5.95%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -6,26 +6,14 @@ public:
     KthLargest(int k, vector<int>& nums) {
         K=k;
         for(int i=0;i<nums.size();i++) {
-            if(pq.size()<k) {
-                pq.push(nums[i]);
-            } else {
-                if(pq.top()<nums[i]) {
-                    pq.pop();
-                    pq.push(nums[i]);
-                }
-            }
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
         }
     }
     
     int add(int val) {
-        if(pq.size()<K) {
-            pq.push(val);
-        } else {
-            if(pq.top()<val) {
-                pq.pop();
-                pq.push(val);
-            }
-        }
+        pq.push(val);
+        if(pq.size()>K) pq.pop();
         return pq.top();
     }
 };
