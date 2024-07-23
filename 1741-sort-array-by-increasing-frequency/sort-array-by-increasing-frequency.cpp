087bd548b3e5dfb7de8f57fc477342f82f995d80
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2024-07-22 18:52:12 -0700

    Time: 9 ms (34.26%) | Memory: 15 MB (37.05%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -2,25 +2,32 @@
 class Solution {
 public:
 
-    static bool compare(pii &a, pii &b ) {
-        if(a.first==b.first) return a.second<b.second;
-        return a.first>b.first;
-    }
+    // static bool compare(pii &a, pii &b ) {
+    //     if(a.first==b.first) return a.second<b.second;
+    //     return a.first>b.first;
+    // }
     vector<int> frequencySort(vector<int>& nums) {
-        vector<int> ans;
+        // vector<int> ans;
         unordered_map<int, int>u;
-        priority_queue<pii, vector<pii>, decltype(&compare)>pq(compare);
+        // priority_queue<pii, vector<pii>, decltype(&compare)>pq(compare);
         for(int i=0;i<nums.size();i++) u[nums[i]]++;
-        for(auto it=u.begin();it!=u.end();it++) {
-            pq.push({it->second, it->first});
-        }
-        while(!pq.empty()) {
-            pii p = pq.top();
-            pq.pop();
-            for(int i=0;i<p.first;i++) {
-                ans.push_back(p.second);
+        // for(auto it=u.begin();it!=u.end();it++) {
+        //     pq.push({it->second, it->first});
+        // }
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if(u[a]==u[b]) {
+                return a>b;
             }
-        }
-        return ans;
+            return u[a]<u[b];
+        });
+        // while(!pq.empty()) {
+        //     pii p = pq.top();
+        //     pq.pop();
+        //     for(int i=0;i<p.first;i++) {
+        //         ans.push_back(p.second);
+        //     }
+        // }
+        // return ans;
+        return nums;
     }
 };
