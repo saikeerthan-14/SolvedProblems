b3a39f477cfdab68d87d4cdcd2f06d1e2d4cbee1
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2024-07-26 16:36:15 -0700

    Time: 29 ms (41.99%), Space: 15.5 MB (75.72%) - LeetHub

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int INM = 100000;
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> G(n, vector<int>(n, INM));
+        for(int i=0;i<edges.size();i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            G[u][v] = G[v][u] = w;
+        }
+        for(int i=0;i<n;i++) {
+            G[i][i] = 0;
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (G[i][j] > (G[i][k] + G[k][j])
+                        && (G[k][j] != INM
+                            && G[i][k] != INM))
+                        G[i][j] = G[i][k] + G[k][j];
+                }
+            }
+        }
+        int ma = 0, mc = INM;
+        for(int i=0;i<n;i++) {
+            int cnt = 0;
+            for(int j=0;j<n;j++) {
+                // if(i==j) G[i][j] = 0;
+                // cout<<G[i][j]<<" ";
+                if(i!=j && G[i][j]<=distanceThreshold) {
+                    cnt++;
+                }
+            }
+            if(cnt<=mc) {
+                mc = cnt;
+                ma = i;
+            }
+            // cout<<endl;
+            // cout<<cnt<<" count"<<endl; 
+            
+        }
+        return ma;
+    }
+
+};
