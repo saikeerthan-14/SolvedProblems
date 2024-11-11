67a23917acc5d9bf8565d3f55fc097903ad9473f
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2024-11-10 19:59:39 -0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (74.92%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -3,8 +3,16 @@ public:
     long long minEnd(int n, int x) {
         long long num = x;
         n--;
-        while(n--) {
-            num = (num+1) | x;
+        for(int i=0;i<64;i++) {
+            
+            if(!(num&(1LL<<i))) {
+                // cout<<(num>>i)<<" - ";
+                num |= (n&1)*(1LL<<i);
+                n = n>>1;
+                // if(n==0) break;
+            }
+            // cout<<(num>>i)<<" "<<((num>>i)&1)<<endl;
+
         }
         return num;
     }
