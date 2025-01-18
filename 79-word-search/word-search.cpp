7738ba8c7ed9738271ee96fcd94f8d11785bb5a5
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2025-01-18 18:51:40 -0500

    Time: 189 ms (91.98%) | Memory: 10.4 MB (84.99%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    int m, n;
     bool exist(vector<vector<char>>& board, string word) {
-    for (unsigned int i = 0; i < board.size(); i++) 
-        for (unsigned int j = 0; j < board[0].size(); j++) 
-            if (dfs(board, i, j, word))
-                return true;
-    return false;
-}
-
-bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-    if (!word.size())
-        return true;
-    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+        m = board.size();
+        n = board[0].size();
+        for (unsigned int i = 0; i < board.size(); i++) 
+            for (unsigned int j = 0; j < board[0].size(); j++) 
+                if (dfs(board, i, j, word, 0))
+                    return true;
         return false;
+    }
+
+bool dfs(vector<vector<char>>& board, int i, int j, string& word, int idx) {
+    if(idx==word.size()) {
+        return true;
+    }
+    
+    if(i>=m || i<0 || j>=n || j<0 || board[i][j]!=word[idx]) return false;
     char c = board[i][j];
     board[i][j] = '*';
-    string s = word.substr(1);
-    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
+    bool ans = dfs(board, i+1, j, word, idx+1) || dfs(board, i-1, j, word, idx+1) || dfs(board, i, j+1, word, idx+1) || dfs(board, i, j-1, word, idx+1);
     board[i][j] = c;
-    return ret;
+    return ans;
 }
 
 };
