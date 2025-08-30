1307e00eb3fb249b01acce635a9f058625a03b6e
Sai Keerthan Palavarapu <52109516+saikeerthan-14@users.noreply.github.com>
2025-08-30 12:32:58 -0700

    Time: 39 ms (38.29%) | Memory: 18.6 MB (44.29%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -3,6 +3,6 @@ class Solution:
         st = 0
         n = len(nums)
         for i in range(n):
-            if i<=st:
+            if i<=st and st<n-1:
                 st = max(st, i+nums[i])
         return st>=n-1
